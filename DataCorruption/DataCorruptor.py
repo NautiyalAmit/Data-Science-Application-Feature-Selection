06833f6ef53f8523a143af65fbe189c0bbc31987
Tomasz Tkaczyk <>
2020-05-26 19:40:38 +0200

    adding cardinal data corruption, changing name and class invocation

--- DataCorruption/DataCorruptor.py
+++ DataCorruption/DataCorruptor.py
@@ -1,22 +1,37 @@
+import warnings
 import pandas as pd
 import numpy as np
 from numpy.random import choice
-import random
-from random import randrange
+import nlpaug.augmenter.char as nac
 
-np.random.seed(10)
-RAND=random.randint(1, 1000)
-#RANDOM=np.random.random()
-    #RANDOM_NORMAL=np.random.normal(row[column_target], sigma, 1)
+from pandas.api.types import is_string_dtype
+from pandas.api.types import is_numeric_dtype
+
+warnings.filterwarnings('ignore')
+
+swapRandom = nac.RandomCharAug(action="swap")
+replaceTwoCharsBasedOnKeyboard = nac.KeyboardAug()
+deleteRandomChar = nac.RandomCharAug(action="delete")
+
+np.random.seed(0)  # Setting seed globally
 
 
-class NumericDataCorruptor:
-    
-    
-    #print("########CLASS############",RAND)
-    def __init__(self, data, feature_stats, feature_cols, log=False):
+# r = np.random.RandomState(0) TODO: Setting the seed for the class locally without impacting global numpy seed
+
+
+class DataCorruptor:
+
+    def __init__(self, data, feature_cols, feature_stats=None, log=False):
+        # np.random.seed(0)
+
+        if feature_stats is None:
+            # TODO: Take the cardinlal statistics (like most common value), into account while corupting data
+            self.feature_stats = data.describe().T[['mean', 'std', 'max', 'min']]
+        else:
+            self.feature_stats = feature_stats
+
         self.data = data.copy()
-        self.feature_stats = feature_stats
+
         self.feature_cols = feature_cols
 
         self.log = log
@@ -24,6 +39,29 @@ class NumericDataCorruptor:
 
         self.corrupted_cells_mask = pd.DataFrame(0, index=np.arange(data.shape[0]), columns=feature_cols)
 
+    def _insert_empty_string(self, row, column_target):
+        if self.log:
+            print(">>> Replacing value with empty string : {}".format(row[column_target]))
+        row[column_target] = ""
+        return row
+
+    def _delete_random_char(self, row, column_target):
+        if self.log:
+            print(">>> Deleting random char: {}".format(row[column_target]))
+        row[column_target] = deleteRandomChar.augment(row[column_target])
+        return row
+
+    def _replace_char_close_on_keyboard(self, row, column_target):
+        if self.log:
+            print(">>> Replacing char with sth close on keyboard {}".format(row[column_target]))
+        row[column_target] = replaceTwoCharsBasedOnKeyboard.augment(row[column_target])
+        return row
+
+    def _swap_random_char(self, row, column_target):
+        if self.log:
+            print(">>> Swaping random char: {}".format(row[column_target]))
+        row[column_target] = swapRandom.augment(row[column_target])
+        return row
 
     def _switch_column_values(self, row, column_target):
         """This function is swiching values between two columns """
@@ -70,13 +108,13 @@ class NumericDataCorruptor:
         """
         # print('introduce outliers')
         sigma, maximum, minimum = self.feature_stats.loc[column_target][['std', 'max', 'min']].to_numpy()
-        if  np.random.random() < 0.5:
-            #print("############INTRODUCE OUTLIER IF ########",RAND)
+        if np.random.random() < 0.5:
+            # print("############INTRODUCE OUTLIER IF ########",RAND)
 
             outlier = maximum + (np.random.random() * sigma)
         else:
-            
-            #print("############INTRODUCE OUTLIER ELSE########",RAND)
+
+            # print("############INTRODUCE OUTLIER ELSE########",RAND)
             outlier = minimum - (np.random.random() * sigma)
         if self.log:
             print('>>> Adding outliers insted of {} => {}'.format(row[column_target], outlier))
@@ -85,16 +123,21 @@ class NumericDataCorruptor:
         return row
 
     def _corrupt_value_by_column(self, row, col_name):
-        #print("########CORRUPT VAL BY COLn############",RAND)
-        if np.random.random() < self.probability_of_error:
-            #TODO: here add functions to corrupt categroical data
+
+        if is_numeric_dtype(self.data[col_name]):
             draw = choice([self._switch_column_values,
                            self._add_noise, self._insert_nan,
                            self._introduce_outlier], 1,
                           p=[0.25, 0.25, 0.25, .25])[0]
             return draw(row, col_name)
-        else:
-            return row
+
+        elif is_string_dtype(self.data[col_name]):
+            draw = choice([self._insert_empty_string,
+                           self._delete_random_char,
+                           self._replace_char_close_on_keyboard,
+                           self._swap_random_char], 1, p=[.25, .25, .25, .25])[0]
+
+            return draw(row, col_name)
 
     def get_dataset_with_corrupted_col(self, col_name, error_proba=0.95):
         """
@@ -113,10 +156,10 @@ class NumericDataCorruptor:
 
     def get_random_indices(self):
         """This function returns a pair for integer indices that are not yet corrupted."""
-        col_ix, row_ix = randrange(self.data.shape[1]), randrange(self.data.shape[0])
+        col_ix, row_ix = np.random.randint(self.data.shape[1]), np.random.randint(self.data.shape[0])
         # While the selected cell is already corrupted, keep "rolling the dice" until we find one that is not corrupted.
-        while self.corrupted_cells_mask.iat[row_ix,col_ix] == 1:
-            col_ix, row_ix = randrange(self.data.shape[1]), randrange(self.data.shape[0])
+        while self.corrupted_cells_mask.iat[row_ix, col_ix] == 1:
+            col_ix, row_ix = np.random.randint(self.data.shape[1]), np.random.randint(self.data.shape[0])
         self.corrupted_cells_mask.iat[row_ix, col_ix] = 1
         return col_ix, row_ix
 
@@ -130,14 +173,15 @@ class NumericDataCorruptor:
         return self.data
 
 
-df = pd.DataFrame([[30, 20, 0.1], [10, 50, 0.5], [15, 30, 0.2]],
-                  columns=['A', 'B', 'C'])
+df = pd.DataFrame([[30, 20, 0.1, 'lol'], [10, 50, 0.5, 'omg'], [15, 30, 0.2, 'wtf']],
+                  columns=['A', 'B', 'C', "D"])
 
-feature_stats = df.describe().T[['mean', 'std', 'max', 'min']]
+data_corruptor = DataCorruptor(df, df.columns.tolist())
 
-data_corruptor = NumericDataCorruptor(df, feature_stats, df.columns.tolist())
+# print(data_corruptor.get_dataset_with_corrupted_cell())
+# print(data_corruptor.get_dataset_with_corrupted_cell())
 
-print(data_corruptor.get_dataset_with_corrupted_cell())
-print(data_corruptor.get_dataset_with_corrupted_cell())
+data_corruptor.get_dataset_with_corrupted_cell()
+data_corruptor.get_dataset_with_corrupted_cell()
 print(data_corruptor.get_dataset_with_corrupted_cell())
 print(data_corruptor.corrupted_cells_mask)
